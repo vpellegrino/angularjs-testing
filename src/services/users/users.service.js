4089a20d840842c7c5012dc4785108973f0b1d22
vpellegr <valentino_pellegrino@bmc.com>
2018-07-23 16:07:52 +0200

    Add documentation on UsersService

--- src/services/users/users.service.js
+++ src/services/users/users.service.js
@@ -1,13 +1,25 @@
+/**
+ * AngularJS service used to handle REST users resources
+ * @author vpellegr
+ */
 angular.module('app.services', [])
     .constant('_', window._)
     .service('UsersService', function ($http, _) {
-        var Users = {};
 
-        Users.getAll = function () {
+        /**
+         * Retrieves all users
+         * @return {Promise<Array>} a promise to return an array of users
+         */
+        this.getAll = function () {
             return _retrieveAllByPredicate(function() { return true });
         };
 
-        Users.findById = function (id) {
+        /**
+         * Retrieves the user that matches a given identifier
+         * @param id {string} user identifier
+         * @return {Promise<Object>} a promise to return a single user
+         */
+        this.findById = function (id) {
             return _retrieveAllByPredicate(function(user) {
                 return user.hasOwnProperty('id') && user.id === id;
             }).then(function (foundUsers) {
@@ -15,6 +27,12 @@ angular.module('app.services', [])
             });
         };
 
+        /**
+         * Retrieves all users and filters them by the given predicate
+         * @param predicate {function} a predicate used to filter returned users
+         * @return {Promise<Array>} a promise to return an array of users
+         * @private
+         */
         function _retrieveAllByPredicate(predicate) {
             return $http({method: 'GET', url: "./users.mocked.json"})
                 .then(function (response) {
@@ -24,5 +42,4 @@ angular.module('app.services', [])
                 });
         }
 
-        return Users;
     });
